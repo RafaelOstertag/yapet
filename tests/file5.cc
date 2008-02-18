2e51df3a5acccd2589da20fd5fc2f9b405f2c51b
Rafael Ostertag <rafi@guengel.ch>
2008-02-18 20:10:48 +0000

    - Test file

---
+++ tests/file5.cc
@@ -0,0 +1,41 @@
+// $Id$
+/*
+ * Depends on the file created by test file3
+ */
+
+#include <string.h>
+
+#include <iostream>
+
+#include <key.h>
+#include <crypt.h>
+#include <structs.h>
+#include <record.h>
+#include <partdec.h>
+#include <file.h>
+
+#include "tests.h"
+
+int main(int, char**) {
+    std::cout << "Be patient, this test may take a few minutes ..." << std::endl;
+    try {
+	YAPET::Key oldkey("JustAPassword");
+	YAPET::Key newkey("JustANewPassword");
+	YAPET::File file(FN, oldkey, false);
+	file.setNewKey(oldkey, newkey);
+	std::list<YAPET::PartDec> list = file.read(newkey);
+	if (list.size() != ROUNDS)
+	    return 1;
+
+	std::list<YAPET::PartDec>::iterator it = list.begin();
+	for(int i=0; it != list.end(); i++) {
+	    check_record(*it, newkey, i);
+	    it++;
+	}
+	
+    } catch (std::exception& ex) {
+	std::cout << ex.what() << std::endl;
+	return 1;
+    }
+    return 0;
+}
