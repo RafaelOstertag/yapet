18748a067d0eeff61e60359fd06bc7bf9127d413
Rafael Ostertag <rafi@guengel.ch>
2008-07-18 20:56:32 +0000

    - Search Dialog

---
+++ yapet/searchdialog.cc
@@ -0,0 +1,141 @@
+// $Id$
+//
+// YAPET -- Yet Another Password Encryption Tool
+// Copyright (C) 2008  Rafael Ostertag
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "searchdialog.h"
+
+void
+SearchDialog::createWindow() throw(YAPETUI::UIException) {
+    if (window != NULL)
+	throw YAPETUI::UIException("May you consider deleting the window before reallocating");
+
+    window = newwin(getHeight(), getWidth(), getStartY(), getStartX());
+    if (window == NULL)
+	throw YAPETUI::UIException("Error creating search dialog");
+
+    searchtermw = new YAPETUI::InputWidget(getStartX() + 1,
+					 getStartY() + 2,
+					 getWidth() - 2);
+    
+    okbutton = new YAPETUI::Button("Ok",
+				   getStartX() + 1,
+				   getStartY() + getHeight() - 2);
+    
+    cancelbutton = new YAPETUI::Button("Cancel",
+				       getStartX() + okbutton->getLength() + 2,
+				       getStartY() + getHeight() - 2);
+}
+
+SearchDialog::SearchDialog() throw(YAPETUI::UIException) : window(NULL),
+							   searchtermw(NULL),
+							   okbutton(NULL),
+							   cancelbutton(NULL),
+							   searchterm(""),
+							   canceled(true) {
+    createWindow();
+}
+
+SearchDialog::~SearchDialog() {
+    delete searchtermw;
+    delete okbutton;
+    delete cancelbutton;
+    delwin(window);
+}
+
+void
+SearchDialog::run() throw(YAPETUI::UIException) {
+    refresh();
+    while (true) {
+	int ch = 0;
+#ifdef HAVE_WRESIZE
+	while ( (ch = searchtermw->focus()) == KEY_RESIZE)
+	    YAPETUI::BaseWindow::resizeAll();
+#else // HAVE_WRESIZE
+	searchtermw->focus();
+#endif // HAVE_WRESIZE
+
+#ifdef HAVE_WRESIZE
+	while ( (ch = okbutton->focus()) == KEY_RESIZE)
+	    YAPETUI::BaseWindow::resizeAll();
+#else // HAVE_WRESIZE
+	ch = okbutton->focus();
+#endif // HAVE_WRESIZE
+	if (ch == '\n') {
+	    canceled = false;
+	    return;
+	}
+#ifdef HAVE_WRESIZE
+	while ( (ch = cancelbutton->focus()) == KEY_RESIZE)
+	    YAPETUI::BaseWindow::resizeAll();
+#else // HAVE_WRESIZE
+	ch = cancelbutton->focus();
+#endif // HAVE_WRESIZE
+	if (ch == '\n') {
+	    canceled = true;
+	    return;
+	}
+    }
+
+}
+
+void
+SearchDialog::resize() throw(YAPETUI::UIException) {
+    int retval = delwin(window);
+    if (retval == ERR)
+	throw YAPETUI::UIException("Error deleting search dialog window");
+
+    delete searchtermw;
+    delete okbutton;
+    delete cancelbutton;
+
+    window = NULL;
+    searchtermw = NULL;
+    okbutton = NULL;
+    cancelbutton = NULL;
+
+    createWindow();
+}
+
+void
+SearchDialog::refresh() throw(YAPETUI::UIException) {
+    YAPETUI::Colors::setcolor(window, YAPETUI::MESSAGEBOX);
+    int retval = werase(window);
+    if (retval == ERR)
+	throw YAPETUI::UIException("Error clearing search dialog");
+
+    retval = box(window, 0, 0);
+    if (retval == ERR)
+	throw YAPETUI::UIException("Error adding box");
+
+    retval = mymvwaddstr(window, 0, 2, "S E A R C H");
+    if (retval == ERR)
+	throw YAPETUI::UIException("Error setting title");
+
+    // The label
+    retval = mymvwaddstr(window, 1, 1, "Please enter the search term");
+    if (retval == ERR)
+	throw YAPETUI::UIException("Error setting label");
+
+    retval = wrefresh(window);
+    if (retval == ERR)
+	throw YAPETUI::UIException("Error refreshing the search dialog");
+
+    searchtermw->refresh();
+    okbutton->refresh();
+    cancelbutton->refresh();
+}
---
+++ yapet/searchdialog.h
@@ -0,0 +1,93 @@
+// -*- c++ -*-
+//
+// $Id$
+//
+// YAPET -- Yet Another Password Encryption Tool
+// Copyright (C) 2008  Rafael Ostertag
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _SEARCHDIALOG_H
+#define _SEARCHDIALOG_H
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#ifdef HAVE_NCURSES_H
+# include <ncurses.h>
+#else // HAVE_NCURSES_H
+# ifdef HAVE_CURSES_H
+#  include <curses.h>
+# else
+#  error "Neither curses.h nor ncurses.h available"
+# endif // HAVE_CURSES_H
+#endif // HAVE_NCURSES_H
+#include "curswa.h" // Leave this here. It depends on the above includes.
+
+#ifdef HAVE_STRING
+# include <string>
+#endif
+
+#include <basewindow.h>
+#include <button.h>
+#include <inputwidget.h>
+
+/**
+ * @brief Search dialog
+ *
+ * Dialog for entering the search term
+ */
+class SearchDialog : protected YAPETUI::BaseWindow {
+    private:
+	enum {
+	    HEIGHT = 6
+	};
+
+	WINDOW* window;
+	YAPETUI::InputWidget* searchtermw;
+	YAPETUI::Button* okbutton;
+	YAPETUI::Button* cancelbutton;
+	YAPETUI::secstring searchterm;
+
+	bool canceled;
+
+	inline SearchDialog(const SearchDialog&) {}
+	inline const SearchDialog& operator=(const SearchDialog&) { 
+	    return *this; }
+
+	inline int getWidth() const { return maxX() - 8; }
+	inline int getHeight() const { return HEIGHT; }
+
+	inline int getStartX() const { return maxX()/2 - getWidth()/2; }
+	inline int getStartY() const { return maxY()/2 - getHeight()/2; }
+
+	void createWindow() throw(YAPETUI::UIException);
+
+    public:
+	SearchDialog() throw (YAPETUI::UIException);
+	~SearchDialog();
+
+	void run() throw(YAPETUI::UIException);
+	void resize() throw(YAPETUI::UIException);
+	void refresh() throw(YAPETUI::UIException);
+
+	inline bool isCanceled() const { return canceled; }
+	inline YAPETUI::secstring getSearchTerm() const {
+	    return searchtermw->getText();
+	}
+};
+
+#endif // _SEARCHDIALOG_H
