9a4d82863ddfd518420495223c2241a448e165f2
Rafael Ostertag <rafi@guengel.ch>
2009-06-11 15:06:39 +0000

    Adjusted the copyright statement as described in the GNU HowTo

--- crypt/key.h
+++ crypt/key.h
@@ -2,21 +2,22 @@
 //
 // $Id$
 //
-// YAPET -- Yet Another Password Encryption Tool
-// Copyright (C) 2008  Rafael Ostertag
+// Copyright (C) 2008, 2009  Rafael Ostertag
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// This file is part of YAPET.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// YAPET is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// YAPET is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// YAPET.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #ifndef _KEY_H
