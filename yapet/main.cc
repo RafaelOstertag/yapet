c8d5ecc2286178e274d41920805e89f303905bec
Rafael Ostertag <rafi@guengel.ch>
2009-07-07 19:18:51 +0000

    updated help text

--- yapet/main.cc
+++ yapet/main.cc
@@ -186,7 +186,7 @@ void show_help(char* prgname) {
     show_version();
     std::cout << std::endl;
     std::cout << prgname
-	      << " [-c] [-h] [-s | -S] [-V] [<filename>]"
+	      << " [-c] [-h] [-s | -S] [-t <sec>] [-V] [<filename>]"
 	      << std::endl
 	      << std::endl;
     std::cout << "-c, --copyright\t\tshow copyright information"
