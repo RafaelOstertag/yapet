082de61aafc1ad88618b298d85c6a28d159f1f5d
Rafael Ostertag <rafi@guengel.ch>
2009-08-18 22:02:40 +0000

    added proper preprocesor directives depending on config.h.

--- yapet/fileopen.cc
+++ yapet/fileopen.cc
@@ -18,14 +18,34 @@
 // YAPET.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <dirent.h>
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#ifdef HAVE_FCNTL_H
+# include <fcntl.h>
+#endif
+#ifdef HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+#ifdef HAVE_ERRNO_H
+# include <errno.h>
+#endif
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
+#ifdef HAVE_STDLIB_H
+# include <stdlib.h>
+#endif
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+#ifdef HAVE_DIRENT_H
+# include <dirent.h>
+#endif
 
 #ifndef HAVE_PATHCONF
 #if HAVE_LIMITS_H
