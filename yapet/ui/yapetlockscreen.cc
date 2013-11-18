2fbb12ff0f388772d81928f4ecbc099b6e450049
Rafael Ostertag <rafi@guengel.ch>
2013-11-18 15:09:15 +0000

    Implemented lockscreen. Not working though.

---
+++ yapet/ui/yapetlockscreen.cc
@@ -0,0 +1,60 @@
+// $Id$
+//
+// Copyright (C) 2008-2013  Rafael Ostertag
+//
+// This file is part of YAPET.
+//
+// YAPET is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// YAPET is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// YAPET.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "../intl.h"
+#include "globals.h"
+#include "yapetlockscreen.h"
+
+//
+// Private
+//
+YapetLockScreen&
+YapetLockScreen::operator=(const YapetLockScreen&) {
+    throw YACURS::EXCEPTIONS::NotSupported();
+    return *this;
+}
+
+//
+// Public
+//
+
+YapetLockScreen::YapetLockScreen(YACURS::Window& mw,
+				 YACURS::UnlockDialog* _unlock,
+				 unsigned int timeout,
+				 unsigned int ulck_timeout) : 
+    LockScreen(_unlock,timeout,ulck_timeout),
+    mainwin(dynamic_cast<MainWindow&>(mw)) {}
+
+YapetLockScreen::~YapetLockScreen() {}
+
+void
+YapetLockScreen::show() {
+    if (YAPET::Globals::file == 0 ||
+	YAPET::Globals::key == 0) {
+	// we cannot lock, there is no open file
+	return;
+    }
+
+    YapetLockScreen::show();
+}
---
+++ yapet/ui/yapetlockscreen.h
@@ -0,0 +1,50 @@
+// -*- c++ -*-
+//
+// $Id$
+//
+// Copyright (C) 2008-2013  Rafael Ostertag
+//
+// This file is part of YAPET.
+//
+// YAPET is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// YAPET is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// YAPET.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _YAPETLOCKSCREEN_H
+#define _YAPETLOCKSCREEN_H 1
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <yacurs.h>
+#include "mainwindow.h"
+
+class YapetLockScreen : public YACURS::LockScreen {
+    private:
+	MainWindow& mainwin;
+
+	// Not supported
+	YapetLockScreen& operator=(const YapetLockScreen&);
+
+    public:
+	YapetLockScreen(YACURS::Window& mw,
+			YACURS::UnlockDialog* _unlock,
+			unsigned int timeout,
+			unsigned int ulck_timeout);
+	virtual ~YapetLockScreen();
+
+	void show();
+};
+
+#endif // _YAPETLOCKSCREEN_H
