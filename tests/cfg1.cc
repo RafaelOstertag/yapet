a5749eaee134230ad92a367f15b26cfc6ac0e51f
Rafael Ostertag <rafi@guengel.ch>
2013-11-23 08:37:31 +0000

    Started working on YAPET::Config test.

---
+++ tests/cfg1.cc
@@ -0,0 +1,21 @@
+// $Id$
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <iostream>
+
+#include "cfg.h"
+#include "testpaths.h"
+
+using namespace YAPET::CONFIG;
+
+int main (int, char**) {
+    Config cfg;
+    
+    abort();
+
+    return 0;
+
+}
