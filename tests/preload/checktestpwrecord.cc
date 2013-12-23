8ca30aab3b2ad0d347ebc7667112013d2f4b4e2a
Rafael Ostertag <rafi@guengel.ch>
2013-12-23 17:50:55 +0000

    Implemented checktestpwrecord.cc.

--- tests/preload/checktestpwrecord.cc
+++ tests/preload/checktestpwrecord.cc
@@ -8,44 +8,86 @@
 #include "crypt.h"
 #include "file.h"
 
-int main() {
-    std::string name("Test Name ");
-    std::string host("Test Host ");
-    std::string user("Test User ");
-    std::string pass("Test Password ");
-    std::string comm("Test Comment ");
+struct control_struct {
+	std::string name;
+	std::string host;
+	std::string user;
+	std::string pass;
+	std::string comm;
+};
+
+std::string name("Test Name ");
+std::string host("Test Host ");
+std::string user("Test User ");
+std::string pass("Test Password ");
+std::string comm("Test Comment ");
+
+std::list<control_struct> control_data;
+YAPET::Key key("pleasechange");
+YAPET::Crypt crypt(key);
+YAPET::File testfile("/tmp/testpwrecord.pet", key, false, false);
+
+bool operator==(const YAPET::PartDec& a, const control_struct& b) {
+    YAPET::Record<YAPET::PasswordRecord>* dec_rec = 
+	crypt.decrypt<YAPET::PasswordRecord>(a.getEncRecord() );
+    YAPET::PasswordRecord* ptr_rec = *dec_rec;
     
+    if (reinterpret_cast<const char*>(ptr_rec->name) != b.name)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->host) != b.host)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->username) != b.user)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->password) != b.pass)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->comment) != b.comm)
+	goto OUTNE;
+	    
+
+    delete dec_rec;
+    return true;
     
-    YAPET::Key key("pleasechange");
-    YAPET::Crypt crypt(key);
-    YAPET::File testfile("/tmp/testpwrecord.pet", key, false, false);
-    
-    std::list<YAPET::PartDec> lst = testfile.read(key);
-    
-    std::list<YAPET::PartDec>::iterator it = lst.begin();
+ OUTNE:
+    delete dec_rec;
+    return false;
+}
 
-    std::list<YAPET::PartDec>::size_type c=0;
-    while (it!=lst.end()) {
+bool operator<(const control_struct& a, const control_struct& b) {
+    return a.name < b.name;
+}
 
-	    YAPET::Record<YAPET::PasswordRecord>* dec_rec = 
-		crypt.decrypt<YAPET::PasswordRecord>(it->getEncRecord() );
-	    YAPET::PasswordRecord* ptr_rec = *dec_rec;
+void 
+init_control_data() {
+    std::ostringstream no;
 
-	    std::ostringstream no;
-	    no.str("");
-	    no.clear();
-	    no << c;
+    for (int i=0; i<100; i++) {
+	no.str("");
+	no.clear();
+	no << i;
 
-	    if ((char*)ptr_rec->name != (name + no.str()))
-		abort();
+	control_struct tmp;
+	tmp.name = name + no.str();
+	tmp.host = host + no.str();
+	tmp.user = user + no.str();
+	tmp.pass = pass + no.str();
+	tmp.comm = comm + no.str();
 
-	    delete dec_rec;
-
-	    c++;
-	    it++;
+	control_data.push_back(tmp);
     }
 
-    if (c!=lst.size()-1)
+    control_data.sort();
+}
+
+int main() {
+    init_control_data();
+
+    std::list<YAPET::PartDec> lst = testfile.read(key);
+    
+    if (control_data.size() != lst.size())
+	abort();
+
+    if (!std::equal(lst.begin(), lst.end(),
+		    control_data.begin()))
 	abort();
 
     std::exit(0);
