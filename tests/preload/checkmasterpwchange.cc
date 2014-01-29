cbdb69bb45f699ae6314da88faca21d68f230c09
Rafael Ostertag <rafi@guengel.ch>
2014-01-29 17:58:45 +0000

    Added two more yapet tests.

---
+++ tests/preload/checkmasterpwchange.cc
@@ -0,0 +1,94 @@
+// $Id$
+//
+// Check the file written by YAPET using the libpwrecord.so preload
+// library
+
+#include <cstdlib>
+#include <sstream>
+#include "crypt.h"
+#include "file.h"
+
+struct control_struct {
+	std::string name;
+	std::string host;
+	std::string user;
+	std::string pass;
+	std::string comm;
+};
+
+std::string name("Test Name ");
+std::string host("Test Host ");
+std::string user("Test User ");
+std::string pass("Test Password ");
+std::string comm("Test Comment ");
+
+std::list<control_struct> control_data;
+YAPET::Key key("pleasechange08");
+YAPET::Crypt yacrypt(key);
+YAPET::File testfile("/tmp/masterpwchange.pet", key, false, false);
+
+bool operator==(const YAPET::PartDec& a, const control_struct& b) {
+    YAPET::Record<YAPET::PasswordRecord>* dec_rec = 
+	yacrypt.decrypt<YAPET::PasswordRecord>(a.getEncRecord() );
+    YAPET::PasswordRecord* ptr_rec = *dec_rec;
+    
+    if (reinterpret_cast<const char*>(ptr_rec->name) != b.name)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->host) != b.host)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->username) != b.user)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->password) != b.pass)
+	goto OUTNE;
+    if (reinterpret_cast<const char*>(ptr_rec->comment) != b.comm)
+	goto OUTNE;
+	    
+
+    delete dec_rec;
+    return true;
+    
+ OUTNE:
+    delete dec_rec;
+    return false;
+}
+
+bool operator<(const control_struct& a, const control_struct& b) {
+    return a.name < b.name;
+}
+
+void 
+init_control_data() {
+    std::ostringstream no;
+
+    for (int i=0; i<100; i++) {
+	no.str("");
+	no.clear();
+	no << i;
+
+	control_struct tmp;
+	tmp.name = name + no.str();
+	tmp.host = host + no.str();
+	tmp.user = user + no.str();
+	tmp.pass = pass + no.str();
+	tmp.comm = comm + no.str();
+
+	control_data.push_back(tmp);
+    }
+
+    control_data.sort();
+}
+
+int main() {
+    init_control_data();
+
+    std::list<YAPET::PartDec> lst = testfile.read(key);
+    
+    if (control_data.size() != lst.size())
+	abort();
+
+    if (!std::equal(lst.begin(), lst.end(),
+		    control_data.begin()))
+	abort();
+
+    std::exit(0);
+}
---
+++ tests/preload/checkyapet.cc
@@ -0,0 +1,89 @@
+// $Id$
+//
+// Create an empty YAPET file, press Enter (nothing must happen),
+// press 'd' (nothing must happen). Quit YAPET.
+//
+//
+// call yapet like this
+//
+//  LD_PRELOAD=libpwrecord.so yapet -i /tmp/testfile
+#include <libyacurscfg.h>
+
+#if defined(HAVE_CURSES_ENHANCED) && \
+    defined(HAVE_LOCALE_H) && \
+    defined(HAVE_CWCHAR) && \
+    !defined(DISABLE_WCHAR)
+# define YACURS_USE_WCHAR 1
+#endif
+
+#include <unistd.h>
+#include <cstdlib>
+
+#ifdef YACURS_USE_WCHAR
+# include <clocale>
+# include <cwchar>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef YACURS_USE_WCHAR
+std::wint_t
+#else
+int
+#endif
+__test_data[50][100] = {
+    { '\n', 0 },
+    // Password for new file
+    { 'p', 'l', 'e', 'a', 's', 'e', 'c', 'h', 'a', 'n', 'g', 'e', '\n', 0 },
+    // Enter password again
+    { 'p', 'l', 'e', 'a', 's', 'e', 'c', 'h', 'a', 'n', 'g', 'e', '\n', 0 },
+    { '\n', 0 }, // Press ok button
+    { '\n', 0 }, // must not do anything
+    { 'd', 0 }, // must not do anything
+    { 'q', 0 }, // quit 
+    { 0 }
+};
+
+#ifdef YACURS_USE_WCHAR
+int
+wget_wch(void* wdc, std::wint_t* i) {
+    static int row = 0;
+    static int col = 0;
+
+    if (__test_data[row][col]==0) {
+	col=0;
+	row++;
+
+	if (__test_data[row][col] == 0)
+	    std::abort();
+    }
+
+    *i = __test_data[row][col++];
+
+    return 0 ;
+}
+
+#else
+int
+wgetch(void* wdc) {
+    static int row = 0;
+    static int col = 0;
+
+    if (__test_data[row][col]==0) {
+	col=0;
+	row++;
+
+	if (__test_data[row] == 0)
+	    std::abort();
+    }
+
+    return __test_data[row][col++];
+}
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
