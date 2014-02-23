89ea7866a540b8db89e6cfb8136c0ecfe830fbe3
Rafael Ostertag <rafi@guengel.ch>
2014-02-23 14:53:56 +0000

    use std::exit().

--- tests/preload/istty.cc
+++ tests/preload/istty.cc
@@ -4,7 +4,7 @@
 int main() {
     // test will not be run if stdout or stdin is not a tty.
     if (isatty(STDOUT_FILENO)!=1 ||
-	isatty(STDIN_FILENO)!=1) exit(1);
+	isatty(STDIN_FILENO)!=1) std::exit(1);
 
-    exit(0);
+    std::exit(0);
 }
