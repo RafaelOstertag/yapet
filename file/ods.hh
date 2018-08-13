5e7f1450fd033a1e80030adcab8d23ef29c95306
Ostertag Rafael <rafael.ostertag@ti8m.ch>
2018-08-13 21:53:42 +0200

    Improve ods and add tests

--- file/ods.hh
+++ file/ods.hh
@@ -7,19 +7,54 @@
 #define _ENDIANESS_HH
 
 #include <cstdint>
+#include "config.h"
 
 namespace yapet {
+namespace {
+template <typename T>
+union Integer {
+    T integer;
+    std::uint8_t bytes[sizeof(T)];
+};
+
+template <typename T>
+void reverseBytes(Integer<T>& integer) {
+    Integer<T> reversed;
+    auto integerSize = sizeof(integer.integer);
+    for (auto i = 0lu; i < integerSize;
+         reversed.bytes[(integerSize - 1) - i] = integer.bytes[i], i++)
+        ;
+    integer.integer = reversed.integer;
+}
+}  // namespace
 
 /**
- * Convert integer to On-Disk-Structure
+ * Convert an integer to On-Disk-Structure
  */
-std::uint32_t toODS(std::uint32_t);
+template <typename T>
+T toODS(T odsInt) {
+#ifdef WORDS_BIGENDIAN
+    return odsInt;
+#else
+    Integer<T> ods{odsInt};
+    reverseBytes(ods);
+    return ods.integer;
+#endif
+}
 
 /**
- * Convert integer from On-Disk-Structure to host representation
+ * Convert an integer from On-Disk-Structure to host representation
  */
-std::uint32_t toHost(std::uint32_t);
-
+template <typename T>
+T toHost(T hostInt) {
+#ifdef WORDS_BIGENDIAN
+    return hostInt;
+#else
+    Integer<T> ods{hostInt};
+    reverseBytes(ods);
+    return ods.integer;
+#endif
+}
 }  // namespace yapet
 
 #endif
