d483b3d004c51191b9be9e01aeb40ca4d874a280
Rafael Ostertag <rafi@guengel.ch>
2018-08-19 15:11:21 +0200

    Add comments

--- file/rawfile.hh
+++ file/rawfile.hh
@@ -10,56 +10,64 @@
 #include "securearray.hh"
 
 namespace yapet {
-class RawFile {
-   private:
-    std::string _filename;
-    FILE* _file;
-    bool _openFlag;
-
-   public:
-    RawFile(const std::string& filename) noexcept;
-    ~RawFile();
-
-    RawFile(const RawFile&) = delete;
-    RawFile& operator=(const RawFile&) = delete;
-
-    RawFile(RawFile&&);
-    RawFile& operator=(RawFile&&);
-
-    void openExisting();
-    void openNew();
-
-    bool isOpen() const { return _openFlag; }
 
     /**
-     * Read \c size bytes.
-     *
-     * The bytes read are returned in a \c SecureArray.
+     * Provide basic file I/O using \c SecureArray 
      */
-    std::pair<SecureArray, bool> read(std::uint32_t size);
-    /**
-     * Read the next record.
-     *
-     * Read the length indicator and return a \c SecureArray holding the data.
-     */
-    std::pair<SecureArray, bool> read();
+    class RawFile {
+    private:
+        std::string _filename;
+        FILE* _file;
+        bool _openFlag;
 
-    /**
-     * Write \c SecureArray to file.
-     *
-     * Write the content of the \c SecureArray preceded by the size of the
-     * record.
-     */
-    void write(const SecureArray& secureArray);
-    /**
-     * Write to file
-     */
-    void write(const std::uint8_t* buffer, std::uint32_t size);
+    public:
+        RawFile(const std::string& filename) noexcept;
+        ~RawFile();
 
-    std::string filename() const { return _filename; }
+        RawFile(const RawFile&) = delete;
+        RawFile& operator=(const RawFile&) = delete;
 
-    void rewind();
-};
-}  // namespace yapet
+        RawFile(RawFile&&);
+        RawFile& operator=(RawFile&&);
+
+        void openExisting();
+        void openNew();
+
+        bool isOpen() const {
+            return _openFlag;
+        }
+
+        /**
+         * Read \c size bytes.
+         *
+         * The bytes read are returned in a \c SecureArray.
+         */
+        std::pair<SecureArray, bool> read(std::uint32_t size);
+        /**
+         * Read the next record.
+         *
+         * Read the length indicator and return a \c SecureArray holding the data.
+         */
+        std::pair<SecureArray, bool> read();
+
+        /**
+         * Write \c SecureArray to file.
+         *
+         * Write the content of the \c SecureArray preceded by the size of the
+         * record.
+         */
+        void write(const SecureArray& secureArray);
+        /**
+         * Write to file. The buffer is written without any preceding size information.
+         */
+        void write(const std::uint8_t* buffer, std::uint32_t size);
+
+        std::string filename() const {
+            return _filename;
+        }
+
+        void rewind();
+    };
+} // namespace yapet
 
 #endif
