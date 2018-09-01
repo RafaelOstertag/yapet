08c74202dece94cabbcef806dd33e6c2e2c31451
Rafael Ostertag <rafi@guengel.ch>
2018-09-01 18:56:42 +0200

    Test blowfish

--- tests/crypt/blowfish.cc
+++ tests/crypt/blowfish.cc
@@ -4,43 +4,62 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
-#include "blowfish.h"
+#include "blowfish.hh"
+#include "key448.hh"
 
 class Header10Test : public CppUnit::TestFixture {
    private:
-    yapet::SecureArray passwordArray{0};
+    std::unique_ptr<yapet::Blowfish> blowfish;
 
    public:
     static CppUnit::TestSuite *suite() {
-        CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite("Key");
+        CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite("Blowfish");
 
         suiteOfTests->addTest(new CppUnit::TestCaller<Header10Test>(
-            "should create proper key", &Header10Test::testKey));
+            "should encrypt and decrypt", &Header10Test::encryptDecrypt));
+
         suiteOfTests->addTest(new CppUnit::TestCaller<Header10Test>(
-            "should create proper IV", &Header10Test::testIV));
+            "should throw on decrypting corrupted data",
+            &Header10Test::decryptCorruptData));
+
+        suiteOfTests->addTest(new CppUnit::TestCaller<Header10Test>(
+            "should throw on empty plain/cipher text",
+            &Header10Test::throwOnEmptyPlainAndCipherText));
 
         return suiteOfTests;
     }
 
     void setUp() {
-        passwordArray = yapet::SecureArray{passwordLength};
-        std::memcpy(*passwordArray, password, passwordLength);
+        std::shared_ptr<yapet::Key> key{new yapet::Key448{}};
+        key->password(yapet::toSecureArray("test"));
+
+        blowfish = std::unique_ptr<yapet::Blowfish>{new yapet::Blowfish{key}};
     }
 
-    void testKey() {
-        YAPET::Key448 key(passwordArray);
+    void encryptDecrypt() {
+        auto plainText{yapet::toSecureArray("Encryption test")};
+        auto cipherText = blowfish->encrypt(plainText);
+        auto actual = blowfish->decrypt(cipherText);
 
-        for (unsigned int i = 0; i < key.keySize(); i++) {
-            CPPUNIT_ASSERT((*key.key())[i] == expected_key[i]);
-        }
+        CPPUNIT_ASSERT(plainText == actual);
     }
 
-    void testIV() {
-        YAPET::Key448 key(passwordArray);
+    void decryptCorruptData() {
+        auto plainText{yapet::toSecureArray("Encryption test")};
+        auto cipherText = blowfish->encrypt(plainText);
 
-        for (unsigned int i = 0; i < key.ivecSize(); i++) {
-            CPPUNIT_ASSERT((*key.ivec())[i] == expected_ivec[i]);
-        }
+        yapet::SecureArray corrupt{cipherText.size() - 2};
+        corrupt << cipherText;
+
+        CPPUNIT_ASSERT_THROW(blowfish->decrypt(corrupt),
+                             YAPET::YAPETEncryptionException);
+    }
+
+    void throwOnEmptyPlainAndCipherText() {
+        yapet::SecureArray empty{};
+
+        CPPUNIT_ASSERT_THROW(blowfish->encrypt(empty), YAPET::YAPETException);
+        CPPUNIT_ASSERT_THROW(blowfish->decrypt(empty), YAPET::YAPETException);
     }
 };
 
