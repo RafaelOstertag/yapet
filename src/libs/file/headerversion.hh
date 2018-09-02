94ce7988696462a4d336d21b3b7859863bcebe99
Rafael Ostertag <rafi@guengel.ch>
2018-09-02 16:54:06 +0200

    Add function to convert int to HEADER_VERSION

--- src/libs/file/headerversion.hh
+++ src/libs/file/headerversion.hh
@@ -1,6 +1,13 @@
 #ifndef _HEADERVERSION_HH
 #define _HEADERVERSION_HH
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "headererror.hh"
+#include "intl.h"
+
 namespace yapet {
 enum HEADER_VERSION {
     /**
@@ -12,6 +19,19 @@ enum HEADER_VERSION {
      */
     VERSION_2 = 2
 };
+
+inline HEADER_VERSION intToHeaderVersion(int version) {
+    switch (version) {
+        case 1:
+            return VERSION_1;
+        case 2:
+            return VERSION_2;
+        default:
+            std::string msg = std::to_string(version);
+            msg += _(" is not a known header version");
+            throw HeaderError(msg.c_str());
+    }
 }
+}  // namespace yapet
 
 #endif
