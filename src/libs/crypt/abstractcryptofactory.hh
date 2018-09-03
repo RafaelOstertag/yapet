b6ace6262227b5d3c875e4ed2780b0a417e0d835
Ostertag Rafael <rafi@guengel.ch>
2018-09-03 20:49:03 +0200

    Add todoS

--- src/libs/crypt/abstractcryptofactory.hh
+++ src/libs/crypt/abstractcryptofactory.hh
@@ -10,8 +10,10 @@ namespace yapet {
 class AbstractCryptoFactory {
    public:
     virtual ~AbstractCryptoFactory() {}
+    //TODO: can we return unique_ptr?
     virtual std::shared_ptr<Crypto> crypto(
         const SecureArray& password) const = 0;
+    // TODO: can we return unique_ptr?
     virtual std::shared_ptr<YapetFile> file(const std::string& filename,
                                             bool create, bool secure) const = 0;
 };
